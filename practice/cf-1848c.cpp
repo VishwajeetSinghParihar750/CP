824b39018b4a5d025191d0a14a22985f2ea8c043
sahil <svishwajeet750@gmail.com>
2025-02-07 18:36:04 +0530

    msg

---
+++ practice/cf-1848c.cpp
@@ -0,0 +1,225 @@
+// Author__ VISHWAJEET_SINGH_PARIHAR __
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+// #define int ll
+typedef int node; // make node whatever you want in o_set
+#define gc getchar_unlocked
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define deb(x) cout << #x << " = " << x << endl
+#define println(n) cout << n << endl
+#define print(n) cout << n << " "
+#define in(n) cin >> n
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+template <typename T>
+T pqInverseMod(T p, T q, T mod)
+{
+    long long expo;
+    expo = mod - 2;
+    while (expo)
+    {
+        if (expo & 1)
+        {
+            p = (p * q) % mod;
+        }
+        q = (q * q) % mod;
+        expo >>= 1;
+    }
+    return p;
+};
+template <typename T>
+T POWER(T a, T b, T mod)
+{
+    ll res = 1;
+    while (b != 0)
+    {
+        if ((b & 1) == 1)
+        {
+            res = (res * a) % mod;
+        }
+        a = (a * a) % mod;
+        b /= 2;
+    }
+    return res;
+}
+template <typename T>
+T gcd(T a, T b)
+{
+    if (b == 0)
+        return a;
+    return (gcd(b, a % b));
+}
+
+//______________________________________________________________________________
+
+const int MOD1 = 1000000007;
+const int MOD2 = 998244353;
+const int inf = INT32_MAX;
+const int NN = 2e5 + 5;
+
+//______________________________________________________________________________
+
+// int a[N];
+
+//___________________________SOLUTION_BEGINS_HERE_______________________________
+
+void solve()
+{
+    int n, m, k, inp;
+    string s;
+
+    cin >> n;
+    vi a(n), b(n), c(n);
+    fo(i, n) cin >> a[i];
+    fo(i, n)
+    {
+        cin >> b[i];
+        c[i] = abs(a[i] - b[i]);
+    }
+
+    //
+
+    vi oper(n);
+    fo(i, n)
+    {
+
+        //
+        // deb(i);
+
+        if (a[i] == 0 and b[i] == 0)
+            oper[i] = 3;
+        else
+        {
+
+            // deb(i), deb(oper[i]);
+            // deb(a[i]), deb(b[i]), deb(c[i]);
+
+            while (a[i] != 0)
+            {
+
+                while (a[i] != 0 and (!(a[i] >= b[i] and b[i] >= c[i])))
+                {
+                    oper[i]++;
+
+                    int temp = abs(a[i] - b[i]);
+
+                    a[i] = b[i];
+                    b[i] = temp;
+
+                    c[i] = abs(a[i] - b[i]); // this is the new c[i] geting made}
+                }
+                //
+
+                if (a[i] != 0)
+                {
+
+                    // deb(a[i]), deb(b[i]), deb(c[i]);
+
+                    if (b[i] == 0)
+                    {
+                        oper[i]++;
+                        break;
+                    }
+                    else if (c[i] == 0)
+                    {
+                        oper[i] += 2;
+                        break;
+                    }
+
+                    int opneeded = b[i] / c[i];
+                    int opadded = opneeded + (opneeded - 1) / 2;
+                    oper[i] += opadded;
+
+                    // deb(opneeded);
+                    // deb((opneeded - 1) / 2);
+
+                    int newc = b[i] % c[i];
+
+                    int opt1 = c[i], opt2 = a[i] - (c[i] * (b[i] / c[i]));
+
+                    int newb = opt1, newa = opt2;
+
+                    if ((opadded % 3) == 2)
+                    {
+
+                        swap(newb, newa);
+                    }
+
+                    // deb(newa), deb(newb), deb(newc);
+
+                    c[i] = newc;
+                    b[i] = newb;
+                    a[i] = newa;
+                }
+                // final rotation of b to a
+            }
+
+            // deb(i), deb(oper[i]);
+
+            oper[i] %= 3;
+        }
+
+        // deb(oper[i]);
+    }
+    set<int> st;
+    fo(i, n)
+    {
+        st.insert(oper[i]);
+    }
+    st.erase(3);
+
+    cout << (st.size() > 1 ? "NO" : "YES") << endl;
+}
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cerr << fixed << setprecision(10);
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int T = 1;
+    in(T);
+    fo(i, T)
+    {
+
+        // cout << "__________________________ TEST CASE " << i << "________________________" << endl;
+        solve();
+    }
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+    // cerr << "Time taken : " << ((long double)duration.count())/((long double) 1e9) <<"s "<< endl;
+
+    return 0;
+}
